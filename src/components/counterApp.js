3a71e8c5b288e8ed2de280af6143a8dd5b1b40d3
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-10-01 17:45:29 +0530

    4

--- src/components/counterApp.js
+++ src/components/counterApp.js
@@ -4,16 +4,18 @@ import {useSelector,useDispatch} from "react-redux";
 import { decrease, increase, } from '../redux/actions/counterActions';
 
 const CounterApp = () => {
-    const count = useSelector(state=>state.count);
-    const dispatch = useDispatch();
+    const count=useSelector(state=>state.count);
+    const dispatch=useDispatch();
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={()=>dispatch(increase())}>Increment</button>
-            <button onClick={()=>dispatch(decrease())}>Decrease</button>
+            <button onClick={()=>dispatch(increase())}>increment</button>
+            <button onClick={()=>dispatch(decrease())}>decrement</button>
 
         </div>
     );
 }
 
 export default CounterApp;
+
+
