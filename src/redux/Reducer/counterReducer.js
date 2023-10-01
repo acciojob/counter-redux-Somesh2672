d65af677acf84ded244773472e97dd2892d4d37e
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-10-01 17:37:02 +0530

    new comite

--- src/redux/Reducer/counterReducer.js
+++ src/redux/Reducer/counterReducer.js
@@ -5,7 +5,7 @@ const couterReducer=(state=initialState,action)=>{
     switch(action.type){
         case 'INCREASE': return {...state,count:state.count+1}
         case 'DECREASE': return {...state,count:state.count-1}
-        default: return initialState;
+        default: return state;
     }
 }
 
